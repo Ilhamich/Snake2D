9c72d05cee70689ab8e0010108e1e347751afa24
Ilhamich <yasniy89@gmail.com>
2022-01-09 18:23:11 +0200

    Changed code style DLSnakeLibrary

--- DLSnakeLibrary/DLWorker.cs
+++ DLSnakeLibrary/DLWorker.cs
@@ -6,29 +6,18 @@ namespace DLSnakeLibrary
 {
     public class DLWorker
     {
-        public static bool ExistSaveFile(string path)
-        {
-            return File.Exists(path);
-        }
+        public static bool ExistSaveFile(string path) =>File.Exists(path);
 
-        public static bool ExistsDir(string dir)
-        {
-            return Directory.Exists(dir);
-        }
-
-        public static void CreatDir(string dir)
-        {
-            Directory.CreateDirectory(dir);
-        }
-
+        public static bool ExistsDir(string dir) => Directory.Exists(dir);
+        
+        public static void CreatDir(string dir) => Directory.CreateDirectory(dir);
+        
         public void Save(string path, GameSnake state)
         {
             BinaryFormatter saver = new BinaryFormatter();
 
             using (FileStream fs = File.Create(path))
-            {
-                saver.Serialize(fs, state);
-            }
+            saver.Serialize(fs, state);
         }
 
         public GameSnake Load(string path)
@@ -37,10 +26,8 @@ namespace DLSnakeLibrary
             BinaryFormatter saver = new BinaryFormatter();
 
             using (FileStream fs = File.Open(path, FileMode.Open))
-            {
-                state = saver.Deserialize(fs) as GameSnake;
-            }
-
+            state = saver.Deserialize(fs) as GameSnake;
+            
             return state;
         }
     }

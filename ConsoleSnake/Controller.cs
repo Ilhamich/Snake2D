7e99becb2a0c24a5825ad7e48651eb22497bfb5c
Ilhamich <yasniy89@gmail.com>
2022-01-15 12:51:38 +0200

    Changed code style Controller

--- ConsoleSnake/Controller.cs
+++ ConsoleSnake/Controller.cs
@@ -57,36 +57,20 @@ namespace ConsoleSnake
         public static InputUser PushButton()
         {
             ConsoleKey key = Console.ReadKey().Key;
-            InputUser result = InputUser.NoDirection;
 
             switch (key)
             {
-                case ConsoleKey.UpArrow:
-                    result = InputUser.UpArrow;
-                    break;
-
-                case ConsoleKey.DownArrow:
-                    result = InputUser.DownArrow;
-                    break;
-
-                case ConsoleKey.Enter:
-                    result = InputUser.Enter;
-                    break;
-
-                default:
-                    break;
+                case ConsoleKey.UpArrow: return  InputUser.UpArrow;
+                case ConsoleKey.DownArrow: return InputUser.DownArrow;
+                case ConsoleKey.Enter: return InputUser.Enter;
+                default: return InputUser.NoDirection;
             }
-
-            return result;
         }
 
         public static void MoveArrow(InputUser key, byte start, byte exit, ref byte stepMenu)
         {
             if (key == InputUser.DownArrow && stepMenu < exit) stepMenu++;
-            else
-            {
-                if (key == InputUser.UpArrow && stepMenu > start) stepMenu--;
-            }
+            else if (key == InputUser.UpArrow && stepMenu > start) stepMenu--;
         }
     }
 }

db0338f05de639fd0f4dc21290372b7a6d2e29fb
Ilhamich <yasniy89@gmail.com>
2022-01-15 12:51:58 +0200

    Changed code style Program

--- ConsoleSnake/Program.cs
+++ ConsoleSnake/Program.cs
@@ -12,19 +12,15 @@ namespace ConsoleSnake
             DLWorker saver = new DLWorker();
             GameSnake game = new GameSnake();
 
-            game.InitGameField(Visualizer.SIDE_SIZE,
-                    Visualizer.FIELD_X_START, Visualizer.FIELD_Y_START);
+            game.InitGameField(Visualizer.SIDE_SIZE, Visualizer.FIELD_X_START, Visualizer.FIELD_Y_START);
 
             do//while (choiceMenu != (byte)MenuChois.ButtonExit);
             {
                 choiceMenu = (byte)MenuChois.ButtonStart;
                 InputUser push = InputUser.NoDirection;
 
-                if (!DLWorker.ExistsDir(game.HomeDir))
-                {
-                    DLWorker.CreatDir(game.HomeDir);
-                }
-
+                if (!DLWorker.ExistsDir(game.HomeDir)) DLWorker.CreatDir(game.HomeDir);
+                
                 string[] menu = new string[]{"START", "LOAD", "DIFFICULTY", "SAVE OPTIONS", "EXIT"};
 
                 Visualizer.PrintField(game.PlayField);
@@ -36,15 +32,11 @@ namespace ConsoleSnake
                     ConnectHelper.WorkWithMenu(menu, ref push, ref choiceMenu);
                 } while (push != InputUser.Enter);
 
-                if (choiceMenu == (byte)MenuChois.ButtonLoad)
-                {
-                    ConnectHelper.WorkWithLoad(ref choiceMenu, saver, ref game);
-                }
-
+                if (choiceMenu == (byte)MenuChois.ButtonLoad) ConnectHelper.WorkWithLoad(ref choiceMenu, saver, ref game);
+                
                 switch (choiceMenu)
                 {
                     case (byte)MenuChois.ButtonStart:
-
                         game.InitSnake(new Coordinate(Visualizer.HEAD_X, Visualizer.HEAD_Y));
                         game.InitFruits();
 
